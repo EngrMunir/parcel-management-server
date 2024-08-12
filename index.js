8a15da8b58df125fb80e37b3ba75ab75efee6654
Munir <smsirajulmonir@gmail.com>
2024-08-12 10:50:12 -0700

    jwt api implemented

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express();
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY)
@@ -32,6 +33,29 @@ async function run() {
     const paymentCollection = client.db('parcelDB').collection('payments');
     const feedbackCollection = client.db('parcelDB').collection('feedback');
 
+    // jwt related api
+    app.post('/jwt',async(req,res)=>{
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET,{expiresIn:'1hr'});
+      res.send({ token });
+    })
+
+    // middleware
+    const verifyToken = (req,res, next)=>{
+      console.log('inside verify token',req.headers.authorization);
+      if(!req.headers.authorization){
+        return res.status(401).send({message:'forbidden access'})
+      }
+      const token = req.headers.authorization.split(' ')[1];
+
+      jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded)=>{
+        if(err){
+          return res.status(401).send({message: 'forbidden access'})
+        }
+        req.decoded =decoded;
+        next();
+      })
+    }
     // my delivery list
     app.get('/myDeliveryList',async(req, res)=>{
       const email = req.query?.email;
@@ -40,7 +64,7 @@ async function run() {
       const deliveryMenId = user._id.toString();
       const filter = { deliveryMenId: deliveryMenId };
       const result = await parcelCollection.find(filter).toArray();
-      console.log(result)
+      // console.log(result)
       res.send(result);     
     })
 
@@ -85,7 +109,7 @@ async function run() {
         }
       }
       const result = await parcelCollection.updateOne(filter,updatedDoc, option)
-      console.log('cancelled', result);
+      // console.log('cancelled', result);
       res.send(result);
     })
 
@@ -114,7 +138,7 @@ async function run() {
         }
       }
       const result = await parcelCollection.updateOne(filter, updatedDoc,option)
-      console.log(result)
+      // console.log(result)
       res.send(result)
     })
 
@@ -122,6 +146,7 @@ async function run() {
     app.patch('/assignDeliveryMen',async(req, res)=>{
       const parcelId = req.body.parcelId;
       const updatedData = req.body;
+      console.log('updated assign info',updatedData);
       const filter = {_id: new ObjectId(parcelId)}
       const option = { upsert: true }
       const updatedDoc ={
@@ -132,14 +157,19 @@ async function run() {
         }
       }
       const result = await parcelCollection.updateOne(filter, updatedDoc,option)
-      console.log(result)
+      // console.log(result)
       res.send(result)
     })
 
     // deliverymen get
+    app.get('/allDeliveryMen', async(req,res)=>{
+      const deliveryMen = await userCollection.find({role:'deliveryMen'}).toArray();
+      res.send(deliveryMen);
+    })
     app.get('/deliveryMen', async(req,res)=>{
       // get all deliveryMen
       const deliveryMen = await userCollection.find({role:'deliveryMen'}).toArray();
+      // console.log(deliveryMen)
 
       // map through each deliveryMen to gather total parcel and average parcel
       const deliveryMenStats = await Promise.all(deliveryMen.map(async (deliveryMen)=>{
@@ -159,6 +189,61 @@ async function run() {
       res.send(deliveryMenStats);
     })
 
+    app.get('/topDeliveryMen', async (req, res) => {
+          const deliveryMen = await userCollection.aggregate([
+              {
+                  $match: { role: 'deliveryMen' } // Filter only delivery men
+              },
+              {
+                  $lookup: {
+                      from: 'parcels',
+                      localField: '_id',
+                      foreignField: 'deliveryMenId',
+                      as: 'parcels'
+                  }
+              },
+              {
+                  $lookup: {
+                      from: 'feedback',
+                      localField: '_id',
+                      foreignField: 'deliveryMenId',
+                      as: 'reviews'
+                  }
+              },
+              {
+                  $addFields: {
+                      parcelCount: { $size: "$parcels" }, // Count the number of parcels
+                      averageRating: {
+                          $cond: {
+                              if: { $eq: [{ $size: "$reviews" }, 0] },
+                              then: 0,
+                              else: { $avg: "$reviews.rating" } // Average the ratings
+                          }
+                      }
+                  }
+              },
+              {
+                  $sort: {
+                      parcelCount: -1,       // Sort by number of parcels delivered (descending)
+                      averageRating: -1      // Then sort by average rating (descending)
+                  }
+              },
+              {
+                  $limit: 3 // Limit to top 3 delivery men
+              },
+              {
+                  $project: {
+                      name: 1,
+                      image: 1,
+                      parcelCount: 1,
+                      averageRating: 1
+                  }
+              }
+          ]).toArray();
+          console.log(deliveryMen);
+          res.send(deliveryMen);
+  });
+  
     // PAYMENT RELATED API
     app.post('/create-payment-intent',async(req, res)=>{
       const { price } = req.body;
@@ -183,9 +268,12 @@ async function run() {
     // feedback related api
     app.get('/feedback',async(req,res)=>{
       const email = req.query.email;
+      // console.log(email)
       const query = {email: email}
       const user = await userCollection.findOne(query);
+      // console.log(user)
       const id = user._id.toString();
+      // console.log('id',id)
       const filter = { deliveryMenId:id}
       const result = await feedbackCollection.find(filter).toArray();
       res.send(result);
